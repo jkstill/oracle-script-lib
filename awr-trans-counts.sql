86217ef016501712373e100f62f263d7e4996054
Jared Still <jkstill@gmail.com>
2016-05-18 11:52:18 -0700

    added sql-command-types.sql and awr-trans-counts.sql

---
+++ awr-trans-counts.sql
@@ -0,0 +1,55 @@
+
+
+-- awr-trans-counts.sql
+-- see: 'How to Calculate the Number of Transactions in a Database (Doc ID 1292114.1)'
+-- Jared Still - still@pythian.com jkstill@gmail.com
+-- Pythian 
+-- 2016-05-18 initial script
+--
+-- also included are redo log synch write waits, as that is the overhead wait caused by commit
+
+set linesize 200 trimspool on
+set pagesize 60
+
+col instance_number head 'I#' format 999
+col begin_interval_time format a30 
+col end_interval_time format a30 
+
+with deltas as (
+select  
+	snap.instance_number
+	, snap.begin_interval_time
+	, snap.end_interval_time
+	, stat.stat_name
+	-- the stat.value and lag_value columns are used for data verification
+	--, stat.value 
+	--, lag(stat.value) 
+		--over (partition by stat.stat_name order by  snap.instance_number, snap.snap_id) 
+	--lag_value
+	, stat.value - 
+		lag(stat.value) 
+		over (partition by stat.stat_name order by  snap.instance_number, snap.snap_id) 
+	stat_delta
+from dba_hist_snapshot snap, dba_hist_sysstat stat
+where snap.instance_number = stat.instance_number
+	and snap.snap_id = stat.snap_id
+	and stat.stat_name in ('user commits','transaction rollbacks','redo synch writes')
+	--and snap.begin_interval_time between timestamp '2016-05-16 00:00:00.000'
+		--and timestamp '2016-05-16 23:59:59.999'
+order by snap.instance_number, stat.stat_name, snap.snap_id
+),
+-- aggregate by day
+sums as (
+select
+	d.instance_number
+	, to_char(trunc(min(d.begin_interval_time)),'yyyy-mm-dd') snap_date
+	, d.stat_name
+	, sum(d.stat_delta) stat_delta
+from deltas d
+group by d.instance_number, trunc(d.begin_interval_time), d.stat_name
+)
+select * from sums s
+order by s.snap_date, s.instance_number
+/
+
+
