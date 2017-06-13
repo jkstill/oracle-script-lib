52497dfd4cee9316c103134e5ea7013c7f717765
Jared Still <jkstill@gmail.com>
2017-06-13 13:37:02 -0700

    Added some timestamp scripts

--- showparmdrvr.sql
+++ showparmdrvr.sql
@@ -3,13 +3,14 @@ set echo off feed on verify off pause off
 
 clear col
 
-set line 100
+set line 110
 col stat new_value statname noprint
 column value format   a50
 column name format   a25
 column isses_modifiable head 'SESS|MOD?' format a4
 column issys_modifiable head 'SYS|MOD?' format a4
 column isdefault head 'DEF|VAL?' format a4
+column inst_id head 'INST' format 9999
 
 set feed off term off
 select '&&1' stat from dual;
@@ -20,6 +21,7 @@ ttitle on
 
 select
 	name
+	, inst_id
 	, value
 	, decode(isdefault, 'TRUE','Y','FALSE','N','?') isdefault
 	, decode(
@@ -33,9 +35,9 @@ select
 		,'DEFERRED','D'
 		,'IMMEDIATE','I'
 		,'?') issys_modifiable
-from v$parameter2
+from gv$parameter2
 where name like '%&&statname%'
-order by name
+order by name, inst_id
 /
 
 
