e41f332276875c9a05e650775a633147d0fa3d95
Jared Still <jkstill@gmail.com>
2017-08-22 17:24:03 -0700

    added concurrent check

--- stats_prefs.sql
+++ stats_prefs.sql
@@ -4,6 +4,8 @@ union all
 select 'ESTIMATE_PERCENT' name, dbms_stats.get_prefs('ESTIMATE_PERCENT') value from dual
 union all
 select 'GRANULARITY' name, dbms_stats.get_prefs('GRANULARITY') value from dual
+union all
+select 'CONCURRENT STATS GATHER' name, dbms_stats.get_prefs('CONCURRENT','SH') from dual
 /
 
 
