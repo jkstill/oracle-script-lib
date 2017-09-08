e193f5ebfa88ba60dcb5e70b0c8a01a702837de2
Jared Still <jkstill@gmail.com>
2017-09-08 07:39:27 -0700

    added lag() to query to get correct statspack values

--- sql/sp_top_sql_io.sql
+++ sql/sp_top_sql_io.sql
@@ -6,9 +6,9 @@
 -- get top 10 SQL statements based on disk reads
 
 with sqlreads as (
-	select distinct sql_id, snap_id, instance_number,  max(disk_reads) over (partition by sql_id) disk_reads
+	select distinct sql_id, snap_id,  instance_number, disk_reads - lag(disk_reads) over (partition by sql_id order by snap_id) disk_reads
 	from perfstat.stats$sql_summary
-	order by 4 desc
+	order by 4 desc nulls last
 )
 select *
 from sqlreads
