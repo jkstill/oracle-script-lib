90e09519d806281ec563f54d2567de0d660520b7
Jared Still <jkstill@gmail.com>
2017-11-25 17:47:35 -0800

    added user input for groupnum

--- sql/asm_partners.sql
+++ sql/asm_partners.sql
@@ -5,6 +5,22 @@
 -- How to Validate Normal or High Redundancy Diskgroups and ASM Disks Partnership (White Paper) (Doc ID 1961372.1)
 -- http://afatkulin.blogspot.com/2012/07/displaying-asm-partner-disks.html
 
+set echo off verify off 
+set term on head on feed on
+
+col u_group_num new_value u_group_num noprint
+
+prompt
+prompt Group Number:
+prompt
+
+set term off feed off
+select '&1' u_group_num from dual;
+set term on feed on
+
+set linesize 200 trimspool on
+set pagesize 100 
+
 column p format a80
 variable group_number number
 exec :group_number := 1;
@@ -21,4 +37,6 @@ from (
 	  and p.grp = :group_number
 	 group by ad1.failgroup, ad1.disk_number, ad2.failgroup
 ) group by d
-order by d;
+order by d
+/
+
