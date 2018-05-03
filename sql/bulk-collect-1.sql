89f4c8dd46a6fb28a715f7dfd22c33598562a662
Jared Still <jkstill@gmail.com>
2018-05-03 15:25:40 -0700

    moved code in body to main()

--- sql/bulk-collect-1.sql
+++ sql/bulk-collect-1.sql
@@ -8,7 +8,21 @@ alter session set plsql_optimize_level=3;
 --alter session set Plsql_Warnings = 'error:all';
 alter session set Plsql_Warnings = 'enable:all';
 
-create or replace package bc
+
+/*
+
+maxrc can be altered at runtime
+
+begin
+  bc.maxrc := 1000;
+  bc.main;
+end;
+/
+
+*/
+
+
+create or replace package bc authid definer
 is
 	maxrc integer := 100;
 	procedure main;
@@ -23,9 +37,6 @@ is
 	type objrec_t is table of dba_objects%rowtype index by binary_integer;
 
 	cursor c1 is select * from dba_objects;
-
-	curr_rows integer := 0;
-	prev_rows integer := 0;
 
 	objects objrec_t;
 
@@ -44,35 +55,32 @@ is
 		dbms_output.new_line;
 	end;
 
-	-- main is just a dummy - we need something to call
 	procedure main
 	is
+		curr_rows integer := 0;
 	begin
-		null;
+		open c1;
+		loop
+
+			fetch c1 bulk collect into objects limit maxrc;
+			exit when c1%notfound;
+
+			pl('rows fetched: ' || to_char(c1%rowcount - curr_rows));
+
+			curr_rows := c1%rowcount;
+
+		end loop;
+
+		pl('rows fetched: ' || to_char(c1%rowcount - curr_rows));
+
+		pl('rowcount: ' || to_char(c1%rowcount));
+
+		close c1;
 	end;
 
 begin
 
 	main;
-
-	open c1;
-	loop
-
-		fetch c1 bulk collect into objects limit maxrc;
-		exit when c1%notfound;
-
-		pl('rows fetched: ' || to_char(c1%rowcount - curr_rows));
-
-		curr_rows := c1%rowcount;
-
-	end loop;
-
-	pl('rows fetched: ' || to_char(c1%rowcount - curr_rows));
-
-	pl('rowcount: ' || to_char(c1%rowcount));
-
-	close c1;
-
 
 end;
 /
