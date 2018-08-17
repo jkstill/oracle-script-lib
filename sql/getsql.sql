0d7fdf7022d24e884a84670d6a38f50dd813b77d
Jared Still <jkstill@gmail.com>
2018-08-17 09:31:33 -0700

    modified:   getsql.sql
      Added ASH/AWR mode
    
    new file:   sql-counts-fms.sql
    
      get sql_id where there are 2+ sql_id per force_matching signature from ASH/AWR
    
    new file:   sql-counts.sql
    
      simple count of SQL_ID from ASH/ASH

--- sql/getsql.sql
+++ sql/getsql.sql
@@ -5,6 +5,11 @@ prompt
 prompt SQLID? :
 prompt
 
+
+-- which ever is an empty string indicates the mode used
+define ash_mode=''
+define awr_mode='--'
+
 col u_sqlid new_value u_sqlid noprint
 
 set term off feed off verify off
@@ -15,14 +20,17 @@ set term on feed on
 @clear_for_spool
 set linesize 32767
 
-col sql_fulltext format a32767
+col sql_text format a32767
 set trimspool on
 set long 1000000
 
 spool sqltext.txt
 
-select sql_fulltext
-from v$sqlstats
+select 
+	&ash_mode sql_fulltext sql_text
+	&awr_mode sql_text
+&ash_mode from v$sqlstats
+&awr_mode from dba_hist_sqltext
 where sql_id = '&&u_sqlid'
 /
 
