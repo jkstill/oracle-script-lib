a9f18720589339d383242d895782804b5947a40e
Jared Still <jkstill@gmail.com>
2018-10-16 15:57:28 -0700

    formatting

--- sql/my-events.sql
+++ sql/my-events.sql
@@ -1,6 +1,12 @@
 
+col event format a40
+col p1text format a40
+
+set linesize 200
+set pagesize 100
+
 select sid, event, p1text, wait_time_micro
 from v$session_wait
-where lower(event) like '%net%'
-and sid = sys_context('userenv','sid')
+where sid = sys_context('userenv','sid')
+--and lower(event) like '%net%'
 /
