810a357fa147171dea7d776b63fc975afb4583a7
Jared Still <jkstill@gmail.com>
2019-01-30 11:44:12 -0800

    asm_files.sql - fixed a typo in join, added missing condition showtrans.sql - look at all instances

--- sql/asm_files.sql
+++ sql/asm_files.sql
@@ -1,6 +1,10 @@
 
 -- asm_files.sql
 -- logon to ASM with sqlplus
+-- currently shows DATAFILES only
+-- does not show aliases for filenames
+-- if system_created = 'N' then it is not a datafile
+-- not using GV view as datafiles are the same between instances
 
 col bytes format 99,999,999,999
 col name format a60
@@ -9,9 +13,10 @@ set line 200 pagesize 60
 select f.file_number, f.bytes, f.modification_date, fn.name
 from v$asm_file f
 join v$asm_alias fn on fn.file_number = f.file_number
-join v$asm_diskgroup dg on f.group_number = f.group_number
-order by bytes asc
+	and fn.group_number = f.group_number
+	and f.type = 'DATAFILE'
+	and fn.system_created = 'Y' -- discriminate datafile from alias
+join v$asm_diskgroup dg on f.group_number = dg.group_number
+order by file_number
 /
-
-
 
--- sql/showtrans.sql
+++ sql/showtrans.sql
@@ -5,6 +5,7 @@ set line 200
 set pagesize 100
 set tab off
  
+col inst_id format 9999 head 'INST|ID'
 col sql_id format a13
 col osuser format a8 heading 'O/S|User' 
 col username format a10 heading 'Oracle|Userid' 
@@ -23,7 +24,9 @@ col command_name format a20 head 'Command|Name'
  
 --spool showtrans.txt
  
-select s.osuser
+select 
+	s.inst_id
+	,s.osuser
 	,s.username
 	,s.sid
 	,c.command_name
@@ -44,16 +47,20 @@ select s.osuser
 	,t.phy_io
 	,s.sql_id
 	,substr(sa.sql_text,1,200) txt
-from v$session s,
-	v$transaction t,
+from gv$session s,
+	gv$transaction t,
 	dba_rollback_segs r,
-	v$sql sa,
+	gv$sql sa,
 	v$sqlcommand c
 where s.saddr=t.ses_addr
+	and s.inst_id = t.inst_id
 and   t.xidusn=r.segment_id(+)
+	and t.inst_id = r.instance_num(+)
 and   s.sql_address=sa.address(+)
+	and s.inst_id = sa.inst_id(+)
 and  s.command = c.command_type
 /
+
 --spool off 
  
 
