0466486283e72c86bb1df74958b534b740c4abbe
Jared Still <jkstill@gmail.com>
2020-03-20 11:23:47 -0700

    remove commas from Description field, do not use quotes as it confuses excel, change magic cookie to find bash

--- bin/all-parms.sh
+++ bin/all-parms.sh
@@ -13,9 +13,9 @@ declare rc=$?
 
 [[ $rc -ne 0 ]] && {
 
-  echo 
+  echo
   echo CMD: $sqlplusCMD not found
-  echo 
+  echo
   echo did you set the Oracle environment?
   echo
 
@@ -58,10 +58,10 @@ cat <<-EOF > $sqlScript
 
 
   set term off feed off head off
-  set pagesize 0 
+  set pagesize 0
   set linesize 300 trimspool on
 
-  set echo off 
+  set echo off
 
   spool &u_logfile
 
@@ -69,24 +69,23 @@ cat <<-EOF > $sqlScript
   -- con_id and inst_id do not seem to be as expected here
 
   select
-    '''' || a.KSPPINM
-    || '''' || ',' || '''' || a.KSPPDESC -- DESCRIPTION
-    || '''' || ',' || '''' || b.KSPFTCTXVL -- VALUE
-    || '''' || ',' || '''' || decode(b.KSPFTCTXDF,'TRUE','Y','N') -- ISDEFAULT
-    || '''' || ',' || '''' || decode(bitand(ksppiflg/256,1),1,'Y' ,'N') -- ISSES_MODIFIABLE
-    || '''' || ',' || '''' || decode(bitand(ksppiflg/65536,3),1,'I',2,'D', 3,'I','N') -- ISSYS_MODIFIABLE
-    || '''' || ',' || '''' || decode(bitand(KSPFTCTXVF,7),1,'M',4,'S','N') -- ISMODIFIED
-    || '''' || ',' || '''' || decode(bitand(KSPFTCTXVF,2),2,'Y','N') -- ISADJUSTED 
-    || ''''
+	 a.KSPPINM
+	 || ',' ||	replace(a.KSPPDESC,',','')	 -- DESCRIPTION
+	 || ',' ||	b.KSPFTCTXVL -- VALUE
+	 || ',' ||	decode(b.KSPFTCTXDF,'TRUE','Y','N') -- ISDEFAULT
+	 || ',' ||	decode(bitand(ksppiflg/256,1),1,'Y' ,'N') -- ISSES_MODIFIABLE
+	 || ',' ||	decode(bitand(ksppiflg/65536,3),1,'I',2,'D', 3,'I','N') -- ISSYS_MODIFIABLE
+	 || ',' ||	decode(bitand(KSPFTCTXVF,7),1,'M',4,'S','N') -- ISMODIFIED
+	 || ','	|| decode(bitand(KSPFTCTXVF,2),2,'Y','N') -- ISADJUSTED
   from X\$KSPPI a, x\$ksppcv2 b
   where b.kspftctxpn = a.indx+1
-    --and  b.inst_id = a.inst_id
-    --and b.con_id = a.con_id
+	 --and  b.inst_id = a.inst_id
+	 --and b.con_id = a.con_id
   order by 1;
 
   spool off
 
-  set term on 
+  set term on
   prompt
   prompt output file: &u_logfile
   prompt
@@ -99,6 +98,3 @@ EOF
 $sqlplusCMD /nolog @${sqlScript}
 
 rm -f ${sqlScript}
-
-
-
