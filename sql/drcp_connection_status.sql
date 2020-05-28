62e385665bb8f46eddafe0f48119e7dd211aebc5
Jared Still <jkstill@gmail.com>
2020-05-28 14:15:38 -0700

    changed v$ to gv$

--- sql/drcp_connection_status.sql
+++ sql/drcp_connection_status.sql
@@ -23,6 +23,6 @@ Might there be an alternative to increase the max count?
 
 select distinct connection_status drcp_connection_status
 	, count(connection_status) over (partition by connection_status) drcp_count
-from V$CPOOL_CONN_INFO
+from GV$CPOOL_CONN_INFO
 order by 1
 /
