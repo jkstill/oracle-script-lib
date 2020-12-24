0ed8ba272cd082653e57a8d81dfcf17bf8a5930f
Jared Still <jkstill@gmail.com>
2020-12-24 09:45:39 -0800

    prompt for SQL_ID - can be passed as parameter. add summary view

--- sql/ash-events.sql
+++ sql/ash-events.sql
@@ -7,9 +7,19 @@
 
 var v_sql_id varchar2(13)
 
-define v_sql_id = '59842mh2aqkwq'
 
-exec :v_sql_id := '&v_sql_id'
+prompt sql_id: 
+
+col u_sql_id new_value u_sql_id noprint
+
+set feed off term off
+select '&1' u_sql_id from dual;
+set feed on term on
+
+exec :v_sql_id := '&u_sql_id'
+
+set linesize 200 trimspool on
+set pagesize 100
 
 col sample_time format a26
 col inst_id format 999999
@@ -26,5 +36,23 @@ and h.event is not null
 group by sample_time, inst_id, session_id, session_serial#, event
 order by sample_time, inst_id, session_id, session_serial#, event
 /
+
+--/*
+
+-- summary view
+
+select  inst_id, event, count(*) event_count
+from gv$active_session_history h
+where h.sql_id = :v_sql_id
+and h.sample_time > sysdate - (2/24)
+and h.event is not null
+group by inst_id, event
+order by count(*);
+
+
+--*/
+
+
+undef 1
 
 
