f9710093a8fb04c8cb45f5d47d43548db5cf9c23
Jared Still <jkstill@gmail.com>
2021-09-10 13:33:33 -0700

    updated columns so no wrapping

--- sql/who2.sql
+++ sql/who2.sql
@@ -38,7 +38,7 @@ col client_program format a20 head 'CLIENT PROGRAM'
 col server_program format a20 head 'SERVER PROGRAM'
 col spid format a5 head 'SRVR|PID'
 col serial# format 99999 head 'SERIAL#'
-col client_process format a12 head 'CLIENT|PID'
+col client_process format a13 head 'CLIENT|PID'
 col osuser format a10
 col logon_time format a17 head 'LOGON TIME'
 col idle_time format a11 head 'IDLE TIME'
