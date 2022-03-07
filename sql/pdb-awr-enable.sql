6ffef4d6726b06125dbd10e5286e84dfe58e5c60
Jared Still <jkstill@gmail.com>
2022-03-07 10:44:37 -0800

    added CDB parameter for randomizing PDB snapshot times

--- sql/pdb-awr-enable.sql
+++ sql/pdb-awr-enable.sql
@@ -24,9 +24,22 @@ end;
 
 whenever sqlerror continue
 
+
 alter system set awr_pdb_autoflush_enabled = true scope=both sid='*';
 
 execute dbms_workload_repository.modify_snapshot_settings(interval => &snapIntervalMinutes)
 
 select SNAP_INTERVAL from cdb_hist_wr_control;
+
+-- set the offset for AWR snapshot generation
+-- the special value of 1000000 causes oracle to choose an offset based on pdb name
+-- https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/AWR_SNAPSHOT_TIME_OFFSET.html#GUID-90CD8379-DCB2-4681-BB90-0A32C6029C4E
+
+prompt
+prompt
+prompt Set this in the CDB to prevent PDBs from running AWR snapshots simultaneously
+prompt
+prompt "alter system set AWR_SNAPSHOT_TIME_OFFSET=1000000 scope=both sid='*';"
+prompt
+prompt
 
