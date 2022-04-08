5ccdd6b65bf5e378917d71d04febcc9995c49101
Jared Still <jkstill@gmail.com>
2022-04-08 13:51:12 -0700

    automatically choose DBA_HIST/CDB/PDB view. exits if no rows

--- sql/awr-event-histogram.sql
+++ sql/awr-event-histogram.sql
@@ -9,18 +9,21 @@ Pass a list of wait classes to ignore
 To see everything pass an empty list 
 ('Idle' is always excluded )
 
-@a ''
+@awr-event-histogram ''
 
-@a '"Other","Cluster"'
+@awr-event-histogram '"Other","Cluster"'
 
-@a '"Other"'
+@awr-event-histogram '"Other"'
 
-@a  '"Other","Cluster","System I/O"'
+@awr-event-histogram  '"Other","Cluster","System I/O"'
 
 The 'all_waits' column is a histgram that represents all chosen  activity.
 
 The values can be quite large, so these are divided by a calculated value for display
 
+Note: this script will exit with an error if logged into a PDB and the AWR_PDB_EVENTS_HISTOGRAM view has zero rows.
+ 
+  running "pdb-awr-enable.sql" may fix that
 
 */
 
@@ -57,13 +60,65 @@ col ignore_list new_value ignore_list noprint
 set term off heading off
 select replace(decode('&1','','''nothing''','&1'),'"','''') ignore_list from dual;
 
+-- get the type of view to use
+var db_type varchar2(30)
+col db_view new_value db_view noprint
+
+declare
+   no_such_parm exception;
+   pragma exception_init(no_such_parm,-2003);
+begin
+   :db_type := sys_context('userenv','con_id');
+exception
+when no_such_parm then
+   :db_type := 0;
+when others then
+   raise;
+end;
+/
+
+select decode(:db_type,
+   0,'DBA_HIST_EVENT_HISTOGRAM',
+   1,'CDB_HIST_EVENT_HISTOGRAM',
+   'AWR_PDB_EVENT_HISTOGRAM') db_view
+from dual
+/
+
+set term on
+set serveroutput on size 1000000
+
+--prompt running rowcheck
+whenever sqlerror exit 1
+
+-- exit if zero rows, such as in a PDB that is not AWR enabled
+declare
+	view_rowcount pls_integer;
+   no_rows exception;
+   pragma exception_init(no_rows,-20000);
+begin
+	select count(*) into view_rowcount from &db_view;
+	--dbms_output.put_line('rows: ' || view_rowcount);
+	if view_rowcount = 0 then
+		dbms_output.put_line('there are 0 rows in &db_view');
+		dbms_output.put_line('if this is a PDB, then the you may need to run "pdb-awr-enable.sql"');
+		raise no_rows;
+	end if;
+end;
+/
+
+whenever sqlerror continue
+set feed off
+
+set serveroutput off
+
 col all_waits_divisor new_value all_waits_divisor noprint
+
 
 -- get the divisor value from the data
 -- all_waits histogram will never be more than 50 characters
 select 
 	floor(max(sum(wait_count)/50))  all_waits_divisor
-from dba_hist_event_histogram
+from &db_view
 where (
 	wait_class not in ('Idle')
 	and
@@ -73,7 +128,10 @@ group by snap_id, instance_number
 order by instance_number, snap_id
 /
 
+
 set term on heading on feed on
+
+prompt Using view &db_view
 
 with wc_symbols as
 (
@@ -117,7 +175,7 @@ rawdata as (
 		, s.wc_symbol
 		, sum(h.wait_time_milli)/60000 wait_time_minutes
 		, sum(h.wait_count) wait_count
-	from dba_hist_event_histogram h
+	from &db_view h
 	join dba_hist_snapshot s on s.snap_id = h.snap_id
 		and h.instance_number = s.instance_number
 		and h.dbid = s.dbid
