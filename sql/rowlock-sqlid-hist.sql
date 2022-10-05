e667666179e4ff81890a5aff1a47b470ecb99f28
Jared Still <jkstill@gmail.com>
2022-10-05 12:35:06 -0700

    updated URL in the comments

--- sql/rowlock-sqlid-hist.sql
+++ sql/rowlock-sqlid-hist.sql
@@ -4,7 +4,7 @@
 -- like rowlock-sqlid-counts.sql, but does full outer join on dba_hist
 -- Jared Still  jkstill@gmail.com
 
--- requires https://github.com/jkstill/oracle-script-lib/blob/master/get_date_range.sql
+-- requires https://github.com/jkstill/oracle-script-lib/blob/master/sql/get_date_range.sql
 
 -- prompt for date range
 --@get_date_range 
