7ff610f104435df1177114b1900c7c52d75d50d6
Jared Still <jkstill@gmail.com>
2023-12-15 09:35:24 -0800

    formatting and order

--- sql/showlog.sql
+++ sql/showlog.sql
@@ -3,7 +3,7 @@
 set line 240 trimspool on
 set pagesize 60
 
-col member format a80 head 'MEMBER'
+col member format a50 head 'MEMBER'
 col group# format 999 head 'GRP'
 col thread# format 999 head 'THR'
 col sequence# format 999999 head 'SEQ'
@@ -32,7 +32,7 @@ select
 from v$log l, v$logfile f
 where l.group# = f.group#
 --and l.inst_id = f.inst_id
-order by thread#,group#
+order by thread#,sequence#
 /
 
 
