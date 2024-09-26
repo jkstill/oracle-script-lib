ad86849109a84bbee37e119a032cbce639aeb46d
Jared Still <jkstill@gmail.com>
2024-09-26 10:28:25 -0700

    removed time_waited from report, as it was incorrect

--- sql/sysevent-top-10.sql
+++ sql/sysevent-top-10.sql
@@ -10,7 +10,6 @@ set trimspool on
 col name format a40
 col total_waits_fg format 99,999,999,999,990
 col time_waited_micro_fg format 9,999,999,999,990.099999
-col time_waited format 99,999,990.099999
 col average_wait_fg format 99,999,990.099999
 col wait_class format a20
 
@@ -33,7 +32,6 @@ select
 	e.name
 	, e.total_waits_fg
 	, e.time_waited_micro_fg
-	, e.time_waited_micro_fg / e.total_waits_fg time_waited
 	, e.average_wait_fg
 	, e.wait_class
 from data e
