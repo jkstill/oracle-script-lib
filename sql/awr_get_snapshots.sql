1646c9d9676edb916c7b7bc30a6c235f539734cd
Jared Still <jkstill@gmail.com>
2026-07-27 15:07:27 -0700

    change to cdb view

--- sql/awr_get_snapshots.sql
+++ sql/awr_get_snapshots.sql
@@ -2,10 +2,17 @@
 
 -- awr_get_snapshots.sql
 
-select instance_number, snap_id, begin_interval_time, end_interval_time
-from dba_hist_snapshot
-where trunc(begin_interval_time,'DD') between to_date('2012-06-27','yyyy-mm-dd') and to_date('2012-06-28','yyyy-mm-dd')
---where rownum < 10
-order by 1,2
+set linesize 200 trimspool on
+set pagesize 1000
+column con_id format 999 heading "Con|ID"
+column dbid format 999999999999 heading "DB|ID"
+column instance_number format 999 heading "Instance|#"
+column snap_id format 9999999 heading "Snap|ID"
+column begin_interval_time format a30 heading "Begin|Interval|Time"
+column end_interval_time format a30 heading "End|Interval|Time"
+
+select con_id, dbid, instance_number, snap_id, begin_interval_time, end_interval_time
+from cdb_hist_snapshot
+order by 1,2,3,4
 /
 
